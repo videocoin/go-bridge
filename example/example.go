70a478b411d8de87952317c76c4dc94a4b9727c4
dmitry <yashulyak@gmail.com>
2020-04-20 09:50:41 +0300

    Add example using private API

--- example/example.go
+++ example/example.go
@@ -9,11 +9,14 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/videocoin/go-bridge/client"
+	"github.com/videocoin/oauth2/google"
+	"golang.org/x/oauth2"
 )
 
 // worker is one of the prepared key for testing workers, prefunded with tokens on goerli network.
-const worker = `{"address":"7bd5ee00a271dbd99f1897768e02e04110a111d6","crypto":{"cipher":"aes-128-ctr","ciphertext":"3b5cd9da1f959d96ccc2426cdfa949ec4e96c360b9b4944ab0cd556016e981ad","cipherparams":{"iv":"59109f4099ec653ca5b49016c8a193a5"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"bd583b0242a8ac1f059f2ddf470e22cfd80d7ff0eae2daa865cdaf287ad717e8"},"mac":"f6b287123c4f222772ea08ec4e5d317d75b08b5c604fa98ca1c0c598dd5f3d14"},"id":"0105a013-e01e-4442-bc05-cb9b2ab31ef4","version":3}`
+const worker = `{"address":"8bc56ed72f5159d6ee385b8fdf0d395279e4f7e6","crypto":{"cipher":"aes-128-ctr","ciphertext":"e3b4f41becf47584ce037376b0eb4c32fb7f268f5d1c3e6a2733d4785c77a711","cipherparams":{"iv":"79557a50a0fb46adcf503f46a682085d"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"a10ec781526d51a93f0a72e8114f3eb97a26cf622ec73f7cd060b57e1a112603"},"mac":"4b805279d58ed1191ecfb9ba65c4bc8dc59b17d92379f2bf78ef941f27346fa4"},"id":"ff2832a5-b98e-4a69-b3bd-661f86062554","version":3}`
 
 func must(err error) {
 	if err != nil {
@@ -21,26 +24,43 @@ func must(err error) {
 	}
 }
 
+func Dial(ctx context.Context, url, clientID, tokenfile string) (*ethclient.Client, error) {
+	ts, err := google.IAPTokenSource(ctx, clientID, tokenfile)
+	if err != nil {
+		return nil, err
+	}
+	r, err := rpc.DialHTTPWithClient(url, oauth2.NewClient(ctx, ts))
+	if err != nil {
+		return nil, err
+	}
+
+	client := ethclient.NewClient(r)
+	return client, nil
+}
+
 func main() {
 	// this is not a regular testable example. this example is using development data that may
 	// get outdated. definitely should not be executed on CI.
-	key, err := keystore.DecryptKey([]byte(worker), "")
+	key, err := keystore.DecryptKey([]byte(worker), "secret")
 
-	goerli, err := ethclient.Dial("https://goerli.infura.io/v3/977d81ca036540f48c4ab19f1927dcd0")
+	goerli, err := ethclient.Dial("https://goerli.infura.io/v3/23b942b35e224604ae314b9ec3f1277c")
 	must(err)
-	vid, err := ethclient.Dial("https://dev1:D6msEL93LJT5RaPk@rpc.dev.kili.videocoin.network")
+	vid, err := Dial(context.TODO(), "https://symphony-everest.dev.videocoin.net/",
+		"47928468404-hfuqhrb6lhtv9sem30rkjc1djcrlpt4v.apps.googleusercontent.com",
+		"/home/dd/Downloads/videocoin-experiments-e8b4d2d00010.json",
+	)
 	must(err)
 	config := client.Config{
-		ProxyAddress:         common.HexToAddress("0x68896bAEcc5186284d5985903e86158F8e803Ca9"),
+		ProxyAddress:         common.HexToAddress("0xc16De466447e348b6Cd1B678d604990e6DB3057C"),
 		ERC20Address:         common.HexToAddress("0x7bc345BE17AF288a0CFcFF8E673714635C847aa0"),
-		LocalBridgeAddress:   common.HexToAddress("0xfD8c66B99919F291AefE756DA506446FB227f17D"),
-		ForeignBridgeAddress: common.HexToAddress("0x06C8031ACa8B5d91Ae52F297511Ca4809aB55e29"),
+		LocalBridgeAddress:   common.HexToAddress("0xb067b9A2eb0bd087F859F836e0AC23E0691Ca62e"),
+		ForeignBridgeAddress: common.HexToAddress("0x3CC38A35E3F93B7C57F44330c9584A48ef98E239"),
 	}
 	client, err := client.Dial(vid, goerli, config)
 	must(err)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
-	tokenbank := common.HexToAddress("0xf90d1852a344a67ca8dabfa1d307b483d89856c4")
+	tokenbank := common.HexToAddress("0x4d80ad6305b893a329039765134ddd436a87ff08")
 
 	info, err := client.WaitDeposit(ctx, key.PrivateKey, tokenbank, big.NewInt(77))
 	must(err)
@@ -50,9 +70,10 @@ func main() {
 
 	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
-	nativebank := common.HexToAddress("0xa2A92CeB62B447A6223fB4f3D2833eE9a40ED9B7")
+	nativebank := common.HexToAddress("0x0406a7288cd7f285f3d339b4c8c8d9a5eeab0b81")
 	info, err = client.WaitWithdraw(ctx, key.PrivateKey, nativebank, big.NewInt(77))
 	must(err)
 	fmt.Printf("Withdraw:\nAddress 0x%x\nVID tx hash 0x%x\nETH tx hash 0x%x\n",
 		key.Address, info.LocalTxHash, info.ForeignTxHash)
+
 }
